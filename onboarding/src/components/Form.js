15f3373e76b7cec51ba9c41df4d5ba9e2249980b
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-07 14:01:12 -0700

    validation

--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
 
-const Basic = () => {
+const Basic = values => {
   return (
     <Form>
       <Field type='text' name='name' placeholder='name' />
       <Field type='text' name='email' placeholder='email' />
       <Field type='password' name='password' placeholder='password' />
-      <Field type='checkbox' name='tos' />
-      <button>submit</button>
+      <label>
+        <Field type="checkbox" name="tos" checked={values.tos} />
+        Terms Of Service
+      </label>
+      <button type='submit'>submit</button>
     </Form>
   );
 }
@@ -16,12 +20,29 @@ const Basic = () => {
 export default withFormik({
   mapPropsToValues(props) {
     return {
-      name: props.name || '',
-      email: props.email || '',
-      password: props.password || '',
+      name: props.name || 'something',
+      email: props.email || 'valid@email.com',
+      password: props.password || 'somethingelse',
       tos: props.tos || false
     };
   },
+
+  validationSchema: Yup.object().shape({
+    name: Yup
+      .string()
+      .required('test'),
+    email: Yup
+      .string()
+      .email()
+      .required(),
+    password: Yup
+      .string()
+      .min(5)
+      .required(),
+    tos: Yup
+      .boolean()
+      .required()
+  }),
 
   handleSubmit(vals) {
     alert(JSON.stringify(vals))
